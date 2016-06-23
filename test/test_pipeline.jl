77eff8d946cd4bcaa4f5f3e0b86d2f414c3b1c14
Joe Greener <jgreener@hotmail.co.uk>
2016-06-23 11:23:04 +0100

    remove Fontconfig requirement

--- test/test_pipeline.jl
+++ test/test_pipeline.jl
@@ -66,4 +66,6 @@ test_pocket_points = testfile("1AKE_pocket_points.pdb")
     )
     @test length(readdir("$temp_dir/pdbs_mod_1")) == 4
     rm(temp_dir, recursive=true)
+
+    @test_throws AssertionError runpipeline(i2=test_i2, d2=test_d2)
 end
